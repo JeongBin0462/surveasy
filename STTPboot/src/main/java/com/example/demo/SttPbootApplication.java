bde906521575344ad930042fd189d78ccfe25100
Jisu-Seong <sjangela0615@gmail.com>
2023-08-11 22:19:28 +0900

    spring boot 프로젝트 연결

---
+++ STTPboot/src/main/java/com/example/demo/SttPbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SttPbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SttPbootApplication.class, args);
+	}
+
+}
