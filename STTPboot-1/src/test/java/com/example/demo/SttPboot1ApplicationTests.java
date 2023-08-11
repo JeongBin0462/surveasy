a9a7c3dfaa6f43e1b11fc0da8acbb070fa5adc38
Jisu-Seong <sjangela0615@gmail.com>
2023-08-12 01:47:46 +0900

    maven

---
+++ STTPboot-1/src/test/java/com/example/demo/SttPboot1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SttPboot1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
