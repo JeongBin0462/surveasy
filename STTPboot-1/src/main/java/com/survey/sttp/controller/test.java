4af2f3d2fa1280379b4374a2fe21dc87582f4203
JeongBin0462 <dlwjdqls783@gmail.com>
2023-08-13 21:43:57 +0900

    페이지 이동 구현

---
+++ STTPboot-1/src/main/java/com/survey/sttp/controller/test.java
@@ -0,0 +1,20 @@
+package com.survey.sttp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class test {
+	
+	@GetMapping(value = "/test")
+	public String start() {
+		return "0.header_footer";
+	}
+	
+	@GetMapping(value = "/test/main")
+	public String main() {
+		
+		return "1.main";
+	}
+}
