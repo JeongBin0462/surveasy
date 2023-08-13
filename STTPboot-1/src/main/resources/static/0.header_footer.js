369497b3b01136ec30bbc4005cb0d86ad819c994
JeongBin0462 <dlwjdqls783@gmail.com>
2023-08-14 01:26:54 +0900

    설문지 작성 내비 마우스 액션 추가

---
+++ STTPboot-1/src/main/resources/static/0.header_footer.js
@@ -0,0 +1,21 @@
+const naviLink = document.querySelector(".naviLink");
+const hiddenDiv = document.querySelector("#hiddenDiv");
+
+naviLink.addEventListener("mouseover", function() {
+    hiddenDiv.style.display = "flex";
+});
+
+document.addEventListener("mousemove", function(event) {
+    const buffer = 10;
+    const naviLinkRect = naviLink.getBoundingClientRect();
+    const hiddenDivRect = hiddenDiv.getBoundingClientRect();
+
+    if (
+        event.clientX < naviLinkRect.left - buffer ||
+        event.clientX > hiddenDivRect.right + buffer ||
+        event.clientY < naviLinkRect.top - buffer ||
+        event.clientY > hiddenDivRect.bottom + buffer
+    ) {
+        hiddenDiv.style.display = "none";
+    }
+});
