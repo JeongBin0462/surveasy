bb043465a2336d025937c6823b88ca4a7f6f23db
JeongBin0462 <dlwjdqls783@gmail.com>
2023-08-20 21:09:51 +0900

    2-2 완성

--- STTPboot-1/src/main/resources/static/2.2survey_make_input.js
+++ STTPboot-1/src/main/resources/static/2.2survey_make_input.js
@@ -1,64 +1,68 @@
-// 여기 위 까지 헤더
-
 document.addEventListener('DOMContentLoaded', function() {
-    const targetSelection = document.getElementById('targetSelection');
-    const employeesInputDiv = document.getElementById('employeesInputDiv');
-    const studentInputDiv = document.getElementById('studentInputDiv');
+	const targetSelection = document.getElementById('targetSelection');
+	const employeesInputDiv = document.getElementById('employeesInputDiv');
+	const studentInputDiv = document.getElementById('studentInputDiv');
 
-    function clearCheckboxes(element) {
-        const checkboxes = element.querySelectorAll('input[type="checkbox"]');
-        checkboxes.forEach(checkbox => {
-            checkbox.checked = false;
-        });
-    }
+	function clearCheckboxes(element) {
+		const checkboxes = element.querySelectorAll('input[type="checkbox"]');
+		checkboxes.forEach(checkbox => {
+			checkbox.checked = false;
+		});
+	}
 
-    function hideElement(element) {
-        element.style.visibility = 'hidden';
-        clearCheckboxes(element);
-    }
+	function hideElement(element) {
+		element.style.visibility = 'hidden';
+		clearCheckboxes(element);
+	}
 
-    function showElement(element) {
-        element.style.visibility = 'visible';
-    }
+	function showElement(element) {
+		element.style.visibility = 'visible';
+	}
 
-    hideElement(employeesInputDiv);
-    hideElement(studentInputDiv);
+	hideElement(employeesInputDiv);
+	hideElement(studentInputDiv);
 
-    targetSelection.addEventListener('change', function() {
-        const selectedValue = this.value;
+	targetSelection.addEventListener('change', function() {
+		const selectedValue = this.value;
 
-        hideElement(employeesInputDiv);
-        hideElement(studentInputDiv);
+		hideElement(employeesInputDiv);
+		hideElement(studentInputDiv);
 
-        if (selectedValue === 'employees') {
-            showElement(employeesInputDiv);
-        } else if (selectedValue === 'student') {
-            showElement(studentInputDiv);
-        }
-    });
+		if (selectedValue === 'employees') {
+			showElement(employeesInputDiv);
+		} else if (selectedValue === 'student') {
+			showElement(studentInputDiv);
+		}
+	});
 
-    targetSelection.dispatchEvent(new Event('change'));
+	targetSelection.dispatchEvent(new Event('change'));
 });
 
 document.addEventListener('DOMContentLoaded', function() {
-    const form = document.querySelector('form');
+	const form = document.querySelector('form');
 
-    form.addEventListener('submit', function(event) {
-        event.preventDefault();
+	form.addEventListener('submit', function(event) {
+		event.preventDefault();
 
-        // 폼 데이터 수집
-        const formData = new FormData(form);
-        let dataObject = {};
-        formData.forEach((value, key) => {
-            dataObject[key] = value;
-        });
+		// 폼 데이터 수집
+		const formData = new FormData(form);
+		let dataObject = {};
+		formData.forEach((value, key) => {
+			dataObject[key] = value;
+		});
 
-        // JSON 문자열 변환 후 세션에 저장
-        const jsonString = JSON.stringify(dataObject);
-        sessionStorage.setItem("formData", jsonString);
+		// JSON 문자열 변환 후 세션에 저장
+		const jsonString = JSON.stringify(dataObject);
+		sessionStorage.setItem("formData", jsonString);
 
-        // 원하는 주소로 리디렉션
-        window.location.href = "/STTP/makesurvey";
-    });
+		// 원하는 주소로 리디렉션
+		window.location.href = "/STTP/makesurvey";
+	});
 });
 
+function toggleCheckbox(divElement) {
+	let checkbox = divElement.querySelector('.checkBox');
+	if (checkbox) {
+		checkbox.checked = !checkbox.checked;
+	}
+}
