68246167dd765feeb34694e2f4d44426b94c5d70
JeongBin0462 <dlwjdqls783@gmail.com>
2023-08-20 20:43:15 +0900

    2-2 css 완성

--- STTPboot-1/src/main/resources/static/2.2survey_make_input.js
+++ STTPboot-1/src/main/resources/static/2.2survey_make_input.js
@@ -1,25 +1,3 @@
-const naviLink = document.querySelector(".naviLink");
-const hiddenDiv = document.querySelector("#hiddenDiv");
-
-naviLink.addEventListener("mouseover", function() {
-	hiddenDiv.style.display = "flex";
-});
-
-document.addEventListener("mousemove", function(event) {
-	const buffer = 10;
-	const naviLinkRect = naviLink.getBoundingClientRect();
-	const hiddenDivRect = hiddenDiv.getBoundingClientRect();
-
-	if (
-		event.clientX < naviLinkRect.left - buffer ||
-		event.clientX > hiddenDivRect.right + buffer ||
-		event.clientY < naviLinkRect.top - buffer ||
-		event.clientY > hiddenDivRect.bottom + buffer
-	) {
-		hiddenDiv.style.display = "none";
-	}
-});
-
 // 여기 위 까지 헤더
 
 document.addEventListener('DOMContentLoaded', function() {
