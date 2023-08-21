393f752679d988b6145e8e50cc7cd0d8a973b71f
JeongBin0462 <dlwjdqls783@gmail.com>
2023-08-22 01:41:42 +0900

    점버튼안에 로직

--- STTPboot-1/src/main/resources/static/2.3survey_make_main.js
+++ STTPboot-1/src/main/resources/static/2.3survey_make_main.js
@@ -2,68 +2,67 @@ let newSurveyBtns = document.getElementsByClassName("newSurveyBtn");
 let newSurveyArea = document.getElementById("survey-question");
 
 function createQuestionElement() {
-    let questionLabel = document.createElement("label");
-    questionLabel.setAttribute("for", "input" + formCount);
-    questionLabel.innerText = "질문 " + formCount;
+  let questionLabel = document.createElement("label");
+  questionLabel.setAttribute("for", "input" + formCount);
+  questionLabel.innerText = "질문 " + formCount;
 
-    let questionContent = document.createElement("input");
-    questionContent.setAttribute("type", "text");
-    questionContent.setAttribute("id", "input" + formCount);
-    questionContent.setAttribute("name", "input" + formCount);
+  let questionContent = document.createElement("input");
+  questionContent.setAttribute("type", "text");
+  questionContent.setAttribute("id", "input" + formCount);
+  questionContent.setAttribute("name", "input" + formCount);
 
-    return {
-        label: questionLabel,
-        content: questionContent
-    };
+  return {
+    label: questionLabel,
+    content: questionContent,
+  };
 }
 
 function createAnswerTypeElement(type, text) {
-    let radio = document.createElement("input");
-    radio.setAttribute("type", "radio");
-    radio.setAttribute("id", type + "Radio" + formCount);
-    radio.setAttribute("name", "answerType" + formCount);
-    radio.setAttribute("value", type + formCount);
+  let radio = document.createElement("input");
+  radio.setAttribute("type", "radio");
+  radio.setAttribute("id", type + "Radio" + formCount);
+  radio.setAttribute("name", "answerType" + formCount);
+  radio.setAttribute("value", type + formCount);
 
-    let label = document.createElement("label");
-    label.setAttribute("for", type + "Radio" + formCount);
-    label.innerText = text;
+  let label = document.createElement("label");
+  label.setAttribute("for", type + "Radio" + formCount);
+  label.innerText = text;
 
-    return {
-        radio: radio,
-        label: label
-    };
+  return {
+    radio: radio,
+    label: label,
+  };
 }
 
 function createForm() {
-    let newForm = document.createElement("form");
-    formCount++;
-    newForm.setAttribute("data-form-number", formCount);
-    newForm.id = "answerForm";
+  let newForm = document.createElement("form");
+  formCount++;
+  newForm.setAttribute("data-form-number", formCount);
+  newForm.id = "answerForm";
 
-    let questionElement = createQuestionElement();
-    newForm.appendChild(questionElement.label);
-    newForm.appendChild(questionElement.content);
+  let questionElement = createQuestionElement();
+  newForm.appendChild(questionElement.label);
+  newForm.appendChild(questionElement.content);
 
-    let answerTypeLabel = document.createElement("label");
-    answerTypeLabel.setAttribute("for", "checkbox" + formCount);
-    answerTypeLabel.innerText = "답변 유형: ";
-    newForm.appendChild(answerTypeLabel);
+  let answerTypeLabel = document.createElement("label");
+  answerTypeLabel.setAttribute("for", "checkbox" + formCount);
+  answerTypeLabel.innerText = "답변 유형: ";
+  newForm.appendChild(answerTypeLabel);
 
-    let types = [
-        { type: "checkbox", text: "체크박스" },
-        { type: "radio", text: "라디오" },
-        { type: "descriptive", text: "서술형" }
-    ];
+  let types = [
+    { type: "checkbox", text: "체크박스" },
+    { type: "radio", text: "라디오" },
+    { type: "descriptive", text: "서술형" },
+  ];
 
-    types.forEach((item) => {
-        let answerTypeElement = createAnswerTypeElement(item.type, item.text);
-        newForm.appendChild(answerTypeElement.radio);
-        newForm.appendChild(answerTypeElement.label);
-    });
+  types.forEach((item) => {
+    let answerTypeElement = createAnswerTypeElement(item.type, item.text);
+    newForm.appendChild(answerTypeElement.radio);
+    newForm.appendChild(answerTypeElement.label);
+  });
 
-    return newForm;
+  return newForm;
 }
-
 
 function createCheckboxRadioListener(checkboxRadio) {
   checkboxRadio.addEventListener("change", function () {
@@ -98,6 +97,117 @@ function createDescriptiveRadioListener(descriptiveRadio) {
   });
 }
 
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// : 버튼 시작점
+
+// 삭제 로직
+function deleteFormLogic(newForm) {
+  newForm.remove();
+
+  // 이후의 모든 form 요소들의 이름 및 아이디 갱신
+  for (let i = formCount; i > newForm.dataset.formNumber; i--) {
+      const formToUpdate = document.querySelector(`[data-form-number='${i}']`);
+      if (formToUpdate) {
+          formToUpdate.setAttribute("data-form-number", i - 1);
+
+          const questionLabel = formToUpdate.querySelector(`[for='input${i}']`);
+          if (questionLabel) {
+              questionLabel.setAttribute("for", "input" + (i - 1));
+              questionLabel.innerText = "질문 " + (i - 1);
+          }
+
+          const questionContent = formToUpdate.querySelector(`#input${i}`);
+          if (questionContent) {
+              questionContent.setAttribute("id", "input" + (i - 1));
+              questionContent.setAttribute("name", "input" + (i - 1));
+          }
+      }
+  }
+  // formCount 값 감소
+  formCount--;
+}
+
+// 삭제 버튼 생성
+function createDeleteButton(newForm) {
+  let deleteButton = document.createElement("button");
+  deleteButton.innerText = "삭제";
+
+  deleteButton.addEventListener("click", function (event) {
+      event.stopPropagation();
+      event.preventDefault();
+      deleteFormLogic(newForm);
+  });
+
+  return deleteButton;
+}
+
+function createImageDiv(newForm) {
+  let imageDiv = document.createElement("div");
+  imageDiv.setAttribute("style", 'width:40px;height:40px;background:url("/static/img/점.png") center/contain no-repeat;cursor:pointer;');
+  imageDiv.setAttribute("id", "imageDiv");
+  addImageDivClickEvent(imageDiv, newForm);
+  newForm.appendChild(imageDiv);
+}
+
+function addImageDivClickEvent(imageDiv, newForm) {
+  let bigDiv;
+  imageDiv.onclick = function () {
+    if (!bigDiv) {
+      bigDiv = createBigDiv(newForm);
+      newForm.appendChild(bigDiv);
+    } else {
+      bigDiv.remove();
+      bigDiv = null;
+    }
+  };
+}
+
+function createBigDiv(newForm) {
+  let bigDiv = document.createElement('div');
+  bigDiv.setAttribute('style', 'border:1px solid black;padding:20px;margin:10px;');
+  bigDiv.appendChild(createDeleteDiv(newForm));
+  bigDiv.appendChild(createRadioDiv());
+  bigDiv.appendChild(createCheckboxDiv());
+  return bigDiv;
+}
+
+function createDeleteDiv(newForm) {
+  let deleteDiv = document.createElement('div');
+  let deleteButton = createDeleteButton(newForm);
+  deleteDiv.appendChild(deleteButton);
+  return deleteDiv;
+}
+
+function createRadioDiv() {
+  let radioDiv = document.createElement('div');
+  let types = [
+    { type: "checkbox", text: "체크박스" },
+    { type: "radio", text: "라디오" },
+    { type: "descriptive", text: "서술형" },
+  ];
+  types.forEach((item) => {
+    let radio = document.createElement('input');
+    radio.setAttribute('type', 'radio');
+    radio.setAttribute('name', 'answerType');
+    radio.setAttribute('value', item.type);
+    
+    let label = document.createElement('label');
+    label.innerText = item.text;
+    radioDiv.appendChild(radio);
+    radioDiv.appendChild(label);
+  });
+  return radioDiv;
+}
+
+function createCheckboxDiv() {
+  let checkboxDiv = document.createElement('div');
+  let checkbox = document.createElement('input');
+  checkbox.setAttribute('type', 'checkbox');
+  checkboxDiv.appendChild(checkbox);
+  return checkboxDiv;
+}
+
+
 let formCount = 0;
 for (let i = 0; i < newSurveyBtns.length; i++) {
   newSurveyBtns[i].addEventListener("click", function (e) {
@@ -117,8 +227,8 @@ for (let i = 0; i < newSurveyBtns.length; i++) {
     // createCheckboxRadioListener(checkboxRadio);
     // createRadioRadioListener(radioRadio);
     // createDescriptiveRadioListener(descriptiveRadio);
-
-    // createDeleteButton(newForm);
+    
+    createImageDiv(newForm);
 
     newSurveyArea.append(newForm);
     newForm.scrollIntoView({ behavior: "smooth" });
