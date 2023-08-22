74a01c2c4391c108b46c0373565d704073f0c115
Jisu-Seong <sjangela0615@gmail.com>
2023-08-22 14:50:48 +0900

    문항추가, 답변추가 임시

--- STTPboot-1/src/main/resources/static/2.3survey_make_main.js
+++ STTPboot-1/src/main/resources/static/2.3survey_make_main.js
@@ -1,269 +1,241 @@
 let newSurveyBtns = document.getElementsByClassName("newSurveyBtn");
+
 let newSurveyArea = document.getElementById("survey-question");
 
-function createQuestionElement() {
-  let questionLabel = document.createElement("label");
-  questionLabel.setAttribute("for", "input" + formCount);
-  questionLabel.innerText = "질문 " + formCount;
+let formCount1 = 0;
+for (let i = 0; i < newSurveyBtns.length; i++) {
+  newSurveyBtns[i].addEventListener("click", function (e) {
+    formCount1++;
 
-  let questionContent = document.createElement("input");
-  questionContent.setAttribute("type", "text");
-  questionContent.setAttribute("id", "input" + formCount);
-  questionContent.setAttribute("name", "input" + formCount);
+    let newDiv = document.createElement("div");
+    newDiv.id = "newDiv" + formCount1;
+    newDiv.className = "newDiv";
+    
+    let newQuestionLabel = document.createElement("p");
+    newQuestionLabel.innerText = formCount1;
+    newQuestionLabel.id = "newQuestionLabel" + newQuestionLabel;
 
-  return {
-    label: questionLabel,
-    content: questionContent,
-  };
-}
+    let inputQuestion = document.createElement("input");
+    inputQuestion.type = "text";
+    inputQuestion.id = "inputQuestion" + formCount1;
 
-function createAnswerTypeElement(type, text) {
-  let radio = document.createElement("input");
-  radio.setAttribute("type", "radio");
-  radio.setAttribute("id", type + "Radio" + formCount);
-  radio.setAttribute("name", "answerType" + formCount);
-  radio.setAttribute("value", type + formCount);
+    let newAnswerBtn = document.createElement("button");
+    newAnswerBtn.type = "button";
+    newAnswerBtn.innerText = "+ 답변 추가";
+    newAnswerBtn.addEventListener("click", function() { createAnswer(newDiv); });
+    newAnswerBtn.id = "newAnswerBtn" + formCount1;
 
-  let label = document.createElement("label");
-  label.setAttribute("for", type + "Radio" + formCount);
-  label.innerText = text;
+    // 새로운 select 요소 생성
+    let selectElem = document.createElement("select");
+    selectElem.className = "answerTypeCombo";
+    selectElem.id = "selectElem" + formCount1;
+        
+    // Option 예제 - 원하는 만큼 option을 추가하십시오.
+    let option1 = document.createElement("option");
+    option1.value = "option1";
+    option1.text = "체크박스";
+    selectElem.appendChild(option1);
 
-  return {
-    radio: radio,
-    label: label,
-  };
-}
+    let option2 = document.createElement("option");
+    option2.value = "option2";
+    option2.text = "라디오버튼";
+    selectElem.appendChild(option2);
 
-function createForm() {
-  let newForm = document.createElement("form");
-  formCount++;
-  newForm.setAttribute("data-form-number", formCount);
-  newForm.id = "answerForm";
+    let option3 = document.createElement("option");
+    option3.value = "option3";
+    option3.text = "서술형";
+    selectElem.appendChild(option3);
 
-  let questionElement = createQuestionElement();
-  newForm.appendChild(questionElement.label);
-  newForm.appendChild(questionElement.content);
-
-  let answerTypeLabel = document.createElement("label");
-  answerTypeLabel.setAttribute("for", "checkbox" + formCount);
-  answerTypeLabel.innerText = "답변 유형: ";
-  newForm.appendChild(answerTypeLabel);
-
-  let types = [
-    { type: "checkbox", text: "체크박스" },
-    { type: "radio", text: "라디오" },
-    { type: "descriptive", text: "서술형" },
-  ];
-
-  types.forEach((item) => {
-    let answerTypeElement = createAnswerTypeElement(item.type, item.text);
-    newForm.appendChild(answerTypeElement.radio);
-    newForm.appendChild(answerTypeElement.label);
-  });
-
-  return newForm;
-}
-
-function createCheckboxRadioListener(checkboxRadio) {
-  checkboxRadio.addEventListener("change", function () {
-    if (checkboxRadio.checked) {
-      // 개수 선택(20개 이하)
-      // input type number
-      // 최소 응답 개수
-      // input type number
-      // 최대 응답 개수
-      // input type number
-    }
+    // 더 많은 옵션을 추가하려면 위의 패턴을 반복하십시오.
+    newDiv.appendChild(newQuestionLabel);
+    newDiv.appendChild(inputQuestion);
+    newDiv.appendChild(selectElem);  // select 요소를 div에 추가
+    newDiv.appendChild(newAnswerBtn);
+  
+    newSurveyArea.append(newDiv);
+    
+    newDiv.scrollIntoView({ behavior: "smooth" });
   });
 }
 
-function createRadioRadioListener(radioRadio) {
-  radioRadio.addEventListener("change", function () {
-    if (radioRadio.checked) {
-      // 1. 각 라디오
-      // 2점척도(예/아니오)
-      // 5점척도
-      // 7점척도
-      // 개수선택(7개이하)
-    }
-  });
+let formCount2 = 0;
+function createAnswer(newDiv) {
+  formCount2++;
+
+  let answerDiv = document.createElement("div");
+  answerDiv.className = "answerDiv";
+  answerDiv.id = "answerDiv" + formCount1 + formCount2;
+
+  // checkbox 요소 생성
+  var checkboxElement = document.createElement("input");
+  checkboxElement.type = "checkbox";
+  checkboxElement.id = "checkboxElement" + formCount1 + formCount2;
+  
+  // input type="text" 요소 생성
+  var inputElement = document.createElement("input");
+  inputElement.type = "text";
+  inputElement.id = "inputElement" + formCount1 + formCount2;
+  
+  // button 요소 생성
+  var buttonElement = document.createElement("button");
+  buttonElement.innerText = "X";
+  buttonElement.id = "buttonElement" + formCount1 + formCount2;
+
+  // buttonElement.addEventListener("click", function() {
+  //   newDiv.removeChild(answerDiv);
+  // });
+  
+  // 요소들을 div에 추가
+  answerDiv.appendChild(checkboxElement);
+  answerDiv.appendChild(inputElement);
+  answerDiv.appendChild(buttonElement);
+
+  newDiv.appendChild(answerDiv);
 }
 
-function createDescriptiveRadioListener(descriptiveRadio) {
-  descriptiveRadio.addEventListener("change", function () {
-    if (descriptiveRadio.checked) {
-      // Your logic here ...
-    }
-  });
-}
+
+
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // : 버튼 시작점
 
 // 삭제 로직
-function deleteFormLogic(newForm) {
-  newForm.remove();
+// function deleteFormLogic(newForm) {
+//   newForm.remove();
 
-  // 이후의 모든 form 요소들의 이름 및 아이디 갱신
-  for (let i = formCount; i > newForm.dataset.formNumber; i--) {
-      const formToUpdate = document.querySelector(`[data-form-number='${i}']`);
-      if (formToUpdate) {
-          formToUpdate.setAttribute("data-form-number", i - 1);
+//   // 이후의 모든 form 요소들의 이름 및 아이디 갱신
+//   for (let i = formCount; i > newForm.dataset.formNumber; i--) {
+//       const formToUpdate = document.querySelector(`[data-form-number='${i}']`);
+//       if (formToUpdate) {
+//           formToUpdate.setAttribute("data-form-number", i - 1);
+		  
 
-          const questionLabel = formToUpdate.querySelector(`[for='input${i}']`);
-          if (questionLabel) {
-              questionLabel.setAttribute("for", "input" + (i - 1));
-              questionLabel.innerText = "질문 " + (i - 1);
-          }
+//           const questionLabel = formToUpdate.querySelector(`[for='input${i}']`);
+//           if (questionLabel) {
+//               questionLabel.setAttribute("for", "input" + (i - 1));
+//               questionLabel.innerText = "질문 " + (i - 1);
+//           }
 
-          const questionContent = formToUpdate.querySelector(`#input${i}`);
-          if (questionContent) {
-              questionContent.setAttribute("id", "input" + (i - 1));
-              questionContent.setAttribute("name", "input" + (i - 1));
-          }
-      }
-  }
-  // formCount 값 감소
-  formCount--;
-}
+//           const questionContent = formToUpdate.querySelector(`#input${i}`);
+//           if (questionContent) {
+//               questionContent.setAttribute("id", "input" + (i - 1));
+//               questionContent.setAttribute("name", "input" + (i - 1));
+//           }
+//       }
+//   }
+//   // formCount 값 감소
+//   formCount--;
+// }
 
-// 삭제 버튼 생성
-function createDeleteButton(newForm) {
-  let deleteButton = document.createElement("button");
-  deleteButton.innerText = "삭제";
+// // 삭제 버튼 생성
+// function createDeleteButton(newForm) {
+//   let deleteButton = document.createElement("button");
+//   deleteButton.innerText = "삭제";
 
-  deleteButton.addEventListener("click", function (event) {
-      event.stopPropagation();
-      event.preventDefault();
-      deleteFormLogic(newForm);
-  });
+//   deleteButton.addEventListener("click", function (event) {
+//       event.stopPropagation();
+//       event.preventDefault();
+//       deleteFormLogic(newForm);
+//   });
 
-  return deleteButton;
-}
+//   return deleteButton;
+// }
 
-function createImageDiv(newForm) {
-  let imageDiv = document.createElement("div");
-  imageDiv.setAttribute("style", 'width:40px;height:40px;background:url("/static/img/점.png") center/contain no-repeat;cursor:pointer;');
-  imageDiv.setAttribute("id", "imageDiv");
-  addImageDivClickEvent(imageDiv, newForm);
-  newForm.appendChild(imageDiv);
-}
+// function createImageDiv(newForm) {
+//   let imageDiv = document.createElement("div");
+//   imageDiv.setAttribute("style", 'width:40px;height:40px;background:url("/static/img/점.png") center/contain no-repeat;cursor:pointer;');
+//   imageDiv.setAttribute("id", "imageDiv");
+//   addImageDivClickEvent(imageDiv, newForm);
+//   newForm.appendChild(imageDiv);
+// }
 
-function addImageDivClickEvent(imageDiv, newForm) {
-  let bigDiv;
-  imageDiv.onclick = function () {
-    if (!bigDiv) {
-      bigDiv = createBigDiv(newForm);
-      newForm.appendChild(bigDiv);
-    } else {
-      bigDiv.remove();
-      bigDiv = null;
-    }
-  };
-}
+// function addImageDivClickEvent(imageDiv, newForm) {
+//   let bigDiv;
+//   imageDiv.onclick = function () {
+//     if (!bigDiv) {
+//       bigDiv = createBigDiv(newForm);
+//       newForm.appendChild(bigDiv);
+//     } else {
+//       bigDiv.remove();
+//       bigDiv = null;
+//     }
+//   };
+// }
 
-function createBigDiv(newForm) {
-  let bigDiv = document.createElement('div');
-  bigDiv.setAttribute('style', 'border:1px solid black;padding:20px;margin:10px;');
-  bigDiv.appendChild(createDeleteDiv(newForm));
-  bigDiv.appendChild(createRadioDiv());
-  bigDiv.appendChild(createCheckboxDiv());
-  return bigDiv;
-}
+// function createBigDiv(newForm) {
+//   let bigDiv = document.createElement('div');
+//   bigDiv.setAttribute('style', 'border:1px solid black;padding:20px;margin:10px;');
+//   bigDiv.appendChild(createDeleteDiv(newForm));
+//   bigDiv.appendChild(createRadioDiv());
+//   bigDiv.appendChild(createCheckboxDiv());
+//   return bigDiv;
+// }
 
-function createDeleteDiv(newForm) {
-  let deleteDiv = document.createElement('div');
-  let deleteButton = createDeleteButton(newForm);
-  deleteDiv.appendChild(deleteButton);
-  return deleteDiv;
-}
+// function createDeleteDiv(newForm) {
+//   let deleteDiv = document.createElement('div');
+//   let deleteButton = createDeleteButton(newForm);
+//   deleteDiv.appendChild(deleteButton);
+//   return deleteDiv;
+// }
 
-function createRadioDiv() {
-  let radioDiv = document.createElement('div');
-  let types = [
-    { type: "checkbox", text: "체크박스" },
-    { type: "radio", text: "라디오" },
-    { type: "descriptive", text: "서술형" },
-  ];
-  types.forEach((item) => {
-    let radio = document.createElement('input');
-    radio.setAttribute('type', 'radio');
-    radio.setAttribute('name', 'answerType');
-    radio.setAttribute('value', item.type);
+// function createRadioDiv() {
+//   let radioDiv = document.createElement('div');
+//   let types = [
+//     { type: "checkbox", text: "체크박스" },
+//     { type: "radio", text: "라디오" },
+//     { type: "descriptive", text: "서술형" },
+//   ];
+//   types.forEach((item) => {
+//     let radio = document.createElement('input');
+//     radio.setAttribute('type', 'radio');
+//     radio.setAttribute('name', 'answerType');
+//     radio.setAttribute('value', item.type);
     
-    let label = document.createElement('label');
-    label.innerText = item.text;
-    radioDiv.appendChild(radio);
-    radioDiv.appendChild(label);
-  });
-  return radioDiv;
-}
+//     let label = document.createElement('label');
+//     label.innerText = item.text;
+//     radioDiv.appendChild(radio);
+//     radioDiv.appendChild(label);
+//   });
+//   return radioDiv;
+// }
 
-function createCheckboxDiv() {
-  let checkboxDiv = document.createElement('div');
-  let checkbox = document.createElement('input');
-  checkbox.setAttribute('type', 'checkbox');
-  checkboxDiv.appendChild(checkbox);
-  return checkboxDiv;
-}
+// function createCheckboxDiv() {
+//   let checkboxDiv = document.createElement('div');
+//   let checkbox = document.createElement('input');
+//   checkbox.setAttribute('type', 'checkbox');
+//   checkboxDiv.appendChild(checkbox);
+//   return checkboxDiv;
+// }
 
+// // 리모컨 로직
+// document.addEventListener("DOMContentLoaded", initializeValues);
 
-let formCount = 0;
-for (let i = 0; i < newSurveyBtns.length; i++) {
-  newSurveyBtns[i].addEventListener("click", function (e) {
-    let newForm = createForm();
+// function initializeValues() {
+//   let answerType = sessionStorage.getItem("answerTypeSelect") || "선택안함";
+//   let inputRequirement =
+//     sessionStorage.getItem("inputRequirementSelect") || "필수입력";
 
-    // Creating listeners for radio buttons:
-    let checkboxRadio = newForm.querySelector(`#checkboxRadio${formCount}`);
-    let radioRadio = newForm.querySelector(`#radioRadio${formCount}`);
-    let descriptiveRadio = newForm.querySelector(
-      `#descriptiveRadio${formCount}`
-    );
+//   document.getElementById("answerTypeSelect").value = answerType;
+//   document.getElementById("inputRequirementSelect").value = inputRequirement;
+// }
 
-    // newForm.appendChild(checkboxRadio);
-    // newForm.appendChild(radioRadio);
-    // newForm.appendChild(descriptiveRadio);
+// function updateSessionStorage(selectElement) {
+//   let key = selectElement.id;
+//   let value = selectElement.value;
 
-    // createCheckboxRadioListener(checkboxRadio);
-    // createRadioRadioListener(radioRadio);
-    // createDescriptiveRadioListener(descriptiveRadio);
-    
-    createImageDiv(newForm);
+//   sessionStorage.setItem(key, value);
+//   console.log("SessionStorage updated with key:", key, "and value:", value);
+// }
 
-    newSurveyArea.append(newForm);
-    newForm.scrollIntoView({ behavior: "smooth" });
-  });
-}
+// // 설정화면 로직
+// function showScreen(screenId) {
+//   // 모든 화면을 숨김
+//   const screens = document.querySelectorAll(".screen");
+//   screens.forEach((screen) => {
+//     screen.classList.remove("active");
+//   });
 
-// 리모컨 로직
-document.addEventListener("DOMContentLoaded", initializeValues);
-
-function initializeValues() {
-  let answerType = sessionStorage.getItem("answerTypeSelect") || "선택안함";
-  let inputRequirement =
-    sessionStorage.getItem("inputRequirementSelect") || "필수입력";
-
-  document.getElementById("answerTypeSelect").value = answerType;
-  document.getElementById("inputRequirementSelect").value = inputRequirement;
-}
-
-function updateSessionStorage(selectElement) {
-  let key = selectElement.id;
-  let value = selectElement.value;
-
-  sessionStorage.setItem(key, value);
-  console.log("SessionStorage updated with key:", key, "and value:", value);
-}
-
-// 설정화면 로직
-function showScreen(screenId) {
-  // 모든 화면을 숨김
-  const screens = document.querySelectorAll(".screen");
-  screens.forEach((screen) => {
-    screen.classList.remove("active");
-  });
-
-  // 선택한 화면만 표시
-  const selectedScreen = document.getElementById(screenId);
-  selectedScreen.classList.add("active");
-}
+//   // 선택한 화면만 표시
+//   const selectedScreen = document.getElementById(screenId);
+//   selectedScreen.classList.add("active");
+// }
