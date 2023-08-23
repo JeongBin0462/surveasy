0493477c3888f11412d329aba2a65b81de892091
LeeJungbin <dlwjdqls783@gmail.com>
2023-08-23 18:29:37 +0900

    회원가입 제작중(오류나오니까 하지마세요)

--- STTPboot-1/src/main/java/com/survey/sttp/controller/user/UserController.java
+++ STTPboot-1/src/main/java/com/survey/sttp/controller/user/UserController.java
@@ -1,15 +1,19 @@
 package com.survey.sttp.controller.user;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.survey.sttp.model.user.UserDTO;
 import com.survey.sttp.service.user.UserService;
 
-@RestController
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
 @RequestMapping("/STTP/user")
+@Slf4j
 public class UserController {
 
     private final UserService userService;
@@ -17,9 +21,22 @@ public class UserController {
     public UserController(UserService userService) {
         this.userService = userService;
     }
+    
+    @GetMapping(value = "/joinagree")
+	public String joinagree() {
+		
+		return "/7.1join_agree";
+	}
+    
+    @GetMapping(value = "/join")
+	public String join() {
+		
+		return "/7.2join";
+	}
 
-    @PostMapping(value = "/join")
-    public int createUser(@RequestBody UserDTO userDTO) {
+    @PostMapping(value = "/join", consumes = "application/json")
+    public String createUser(@RequestBody UserDTO userDTO) {
+    	log.info(userDTO.toString());
         return userService.insertUser(userDTO);
     }
 }
--- STTPboot-1/src/main/java/com/survey/sttp/service/user/UserService.java
+++ STTPboot-1/src/main/java/com/survey/sttp/service/user/UserService.java
@@ -3,5 +3,5 @@ package com.survey.sttp.service.user;
 import com.survey.sttp.model.user.UserDTO;
 
 public interface UserService {
-    int insertUser(UserDTO userDTO);
+    String insertUser(UserDTO userDTO);
 }
