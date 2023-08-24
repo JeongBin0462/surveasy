7bfac887fdf0c3aa0c16f8fb4ce39c9caf9621fd
Jisu-Seong <sjangela0615@gmail.com>
2023-08-25 01:04:37 +0900

    js 정리

--- STTPboot-1/src/main/resources/static/2.3survey_make_main.js
+++ STTPboot-1/src/main/resources/static/2.3survey_make_main.js
@@ -1,10 +1,6 @@
 // 답변 개수 확인 함수
 function checkAnswerCount(type, newAnswerBtn) {
-  console.log("Type:", type);
-  console.log("answerCounts:", answerCounts[type]);
-   console.log("maxAnswerCounts:", maxAnswerCounts[type]);
-
-  if (answerCounts[type] > maxAnswerCounts[type]) {
+  if (currentAnswerCounts[type] > maxAnswerCounts[type]) {
     newAnswerBtn.style.display = "none";
   } else {
     newAnswerBtn.style.display = "block";
@@ -12,7 +8,7 @@ function checkAnswerCount(type, newAnswerBtn) {
 }
 
 // 현재 답변 개수를 저장하는 객체
-const answerCounts = {
+const currentAnswerCounts = {
   체크박스: 0,
   라디오버튼: 0,
   서술형: 0,
@@ -31,15 +27,135 @@ let newSurveyArea = document.getElementById("survey-question");
 // 질문 번호 카운트용
 let formCount1 = 0;
 
+// 설문타입변경리스너
+function surveyTypeChange(event) {
+  let selectElem = event.currentTarget;
+  let topDiv = selectElem.parentElement;
+
+  // 모든 답변을 제거
+  let newAnswerBtn = topDiv.querySelector(".btn_addAnswer");
+  let answers = topDiv.querySelectorAll(".answerContainer");
+  answers.forEach((answer) => topDiv.removeChild(answer));
+
+  // 새로운 유형에 따라 답변 개수 확인
+  let selectedText = selectElem.options[selectElem.selectedIndex].text;
+
+  // currentAnswerCounts의 해당 타입의 개수를 0으로 초기화
+  let currentAnswerCounts = JSON.parse(topDiv.dataset.answerCounts);
+  currentAnswerCounts[selectedText] = 0;
+
+  // 변경된 currentAnswerCounts를 topDiv의 data-answerCounts 속성에 저장
+  topDiv.dataset.answerCounts = JSON.stringify(currentAnswerCounts);
+
+  // 버튼의 표시 상태를 초기화
+  newAnswerBtn.style.display = "block";
+  checkAnswerCount(selectedText, newAnswerBtn);
+
+  // 요소들이 topDiv에 이미 존재하는지 확인
+  let minLabelExists = topDiv.querySelector("label.minLabel");
+  let maxLabelExists = topDiv.querySelector("label.maxLabel");
+  let minRequiredExists = topDiv.querySelector("input.input_minRequired");
+  let maxRequiredExists = topDiv.querySelector("input.input_maxRequired");
+  let isdiscriptionExampleExists = topDiv.querySelector(".answerDesc");
+
+  if (selectedText == "서술형") {
+    var discriptionExample = document.createElement("label");
+    discriptionExample.className = "answerDesc";
+    topDiv.appendChild(discriptionExample);
+  } else {
+    if (isdiscriptionExampleExists)
+      topDiv.removeChild(isdiscriptionExampleExists);
+  }
+
+  if (selectedText != "체크박스") {
+    if (minLabelExists) topDiv.removeChild(minLabelExists);
+    if (maxLabelExists) topDiv.removeChild(maxLabelExists);
+    if (minRequiredExists) topDiv.removeChild(minRequiredExists);
+    if (maxRequiredExists) topDiv.removeChild(maxRequiredExists);
+    // if (answerDiv) answerDiv.remove();
+  } else {
+    // 요소들이 이미 존재하지 않는 경우에만 추가
+    if (!minLabelExists) {
+      var minLabel = document.createElement("label");
+      minLabel.textContent = "최소 선택 가능 개수: ";
+      minLabel.className = "minLabel";
+      topDiv.appendChild(minLabel);
+    }
+
+    if (!minRequiredExists) {
+      var minRequired = document.createElement("input");
+      minRequired.type = "number";
+      minRequired.className = "input_minRequired";
+      minRequired.min = "1";
+      minRequired.max = "20";
+      topDiv.appendChild(minRequired);
+    }
+
+    if (!maxLabelExists) {
+      var maxLabel = document.createElement("label");
+      maxLabel.textContent = "최대 선택 가능 개수: ";
+      maxLabel.className = "maxLabel";
+      topDiv.appendChild(maxLabel);
+    }
+
+    if (!maxRequiredExists) {
+      var maxRequired = document.createElement("input");
+      maxRequired.type = "number";
+      maxRequired.className = "input_maxRequired";
+      maxRequired.min = "1";
+      maxRequired.max = "20";
+      topDiv.appendChild(maxRequired);
+    }
+  }
+}
+
+// 답변추가리스너
+function addAnswer(event) {
+  let topDiv = event.currentTarget.parentElement;
+  let currentAnswerCounts = JSON.parse(topDiv.dataset.answerCounts); // 가져오기
+  let selectElem = topDiv.querySelector(".answerTypeCombo");
+
+  // 현재의 답변 유형 텍스트 추출
+  let selectedIndex = selectElem.selectedIndex;
+  let selectedText = selectElem.options[selectedIndex].text;
+
+  currentAnswerCounts[selectedText]++; // 수정하기
+  console.log("answerCounts 상태: " + currentAnswerCounts[selectedText]);
+
+  topDiv.dataset.answerCounts = JSON.stringify(currentAnswerCounts); // 저장하기
+  checkAnswerCount(selectedText, event.currentTarget);
+
+  // 답변 생성 함수 호출
+  createAnswer(
+    topDiv,
+    formCount1,
+    selectedText, // 수정된 부분
+    selectElem,
+    event.currentTarget
+  );
+}
+
 // newSurveyBtns는 여러개 있음(본문 1개, 리모컨 1개)
 for (let i = 0; i < newSurveyBtns.length; i++) {
   // 질문 추가 리스너
   newSurveyBtns[i].addEventListener("click", function (e) {
     formCount1++;
 
+    // 리모컨 요소
+    let answerTypeSelectValue =
+      document.getElementById("answerTypeSelect").value;
+
     // 설문 질문 컨테이너 생성
     var topDiv = document.createElement("div");
     topDiv.className = "topDiv";
+
+    var topDivAnswerCounts = {
+      체크박스: 0,
+      라디오버튼: 0,
+      서술형: 0,
+    };
+
+    topDiv.dataset.answerCounts = JSON.stringify(topDivAnswerCounts);
 
     // 질문 레이블 생성
     let newQuestionLabel = document.createElement("p");
@@ -50,6 +166,7 @@ for (let i = 0; i < newSurveyBtns.length; i++) {
     let inputQuestion = document.createElement("input");
     inputQuestion.type = "text";
     inputQuestion.className = "inputQuestion";
+    inputQuestion.value = "질문 내용 입력";
 
     // 답변 추가 버튼 생성
     let newAnswerBtn = document.createElement("button");
@@ -89,6 +206,20 @@ for (let i = 0; i < newSurveyBtns.length; i++) {
     option3.text = "서술형";
     selectElem.appendChild(option3);
 
+    switch (answerTypeSelectValue) {
+      case "체크박스":
+        selectElem.value = "check";
+        break;
+      case "라디오버튼":
+        selectElem.value = "rad";
+        break;
+      case "서술형":
+        selectElem.value = "desc";
+        break;
+      default:
+        break;
+    }
+
     // topDiv에 요소들 추가
     topDiv.appendChild(newQuestionLabel);
     topDiv.appendChild(inputQuestion);
@@ -96,105 +227,29 @@ for (let i = 0; i < newSurveyBtns.length; i++) {
     topDiv.appendChild(newAnswerBtn);
 
     // topDiv와 bottomDiv를 감싸는 totalDiv 생성
-    let totalDiv = document.createElement("div");
-    totalDiv.className = "questionContainer";
+    // let totalDiv = document.createElement("div");
+    // totalDiv.className = "questionContainer";
 
     // bottomDiv 생성
-    let bottomDiv = createBottomDiv(totalDiv);
+    let inputRequirement = document.getElementById("inputRequireSelect").value;
+    let isMandatory = inputRequirement === "필수입력" ? true : false;
+    let bottomDiv = createBottomDiv(topDiv, isMandatory);
 
     // totalDiv에 topDiv, bottomDiv 추가
-    totalDiv.appendChild(topDiv);
-    totalDiv.appendChild(bottomDiv);
+    // totalDiv.appendChild(topDiv);
+    topDiv.appendChild(bottomDiv);
 
     // newSurveyArea에 totalDiv 추가
-    newSurveyArea.append(totalDiv);
+    newSurveyArea.append(topDiv);
 
     // topDiv 생성시 스크롤 포커싱
-    totalDiv.scrollIntoView({ behavior: "smooth" });
+    topDiv.scrollIntoView({ behavior: "smooth" });
 
     // 답변유형 콤보박스 리스너
-    selectElem.addEventListener("change", function () {
-      // 모든 답변을 제거
-      let answers = topDiv.querySelectorAll(".answerContainer");
-      answers.forEach((answer) => topDiv.removeChild(answer));
-
-      // 답변 카운트 초기화
-      answerCounts["체크박스"] = 0;
-      answerCounts["라디오버튼"] = 0;
-      answerCounts["서술형"] = 0;
-
-      // 새로운 유형에 따라 답변 개수 확인
-      checkAnswerCount(
-        selectElem.options[selectElem.selectedIndex].text,
-        newAnswerBtn
-      );
-
-      // 요소들이 topDiv에 이미 존재하는지 확인
-      let minLabelExists = topDiv.querySelector("label.minLabel");
-      let maxLabelExists = topDiv.querySelector("label.maxLabel");
-      let minRequiredExists = topDiv.querySelector("input.input_minRequired");
-      let maxRequiredExists = topDiv.querySelector("input.input_maxRequired");
-
-      if (selectElem.options[selectElem.selectedIndex].text != "체크박스") {
-        if (minLabelExists) topDiv.removeChild(minLabelExists);
-        if (maxLabelExists) topDiv.removeChild(maxLabelExists);
-        if (minRequiredExists) topDiv.removeChild(minRequiredExists);
-        if (maxRequiredExists) topDiv.removeChild(maxRequiredExists);
-        // if (answerDiv) answerDiv.remove();
-      } else {
-        // 요소들이 이미 존재하지 않는 경우에만 추가
-        if (!minLabelExists) {
-          var minLabel = document.createElement("label");
-          minLabel.textContent = "최소 선택 가능 개수: ";
-          minLabel.className = "minLabel";
-          topDiv.appendChild(minLabel);
-        }
-
-        if (!minRequiredExists) {
-          var minRequired = document.createElement("input");
-          minRequired.type = "number";
-          minRequired.className = "input_minRequired";
-          minRequired.min = "1";
-          minRequired.max = "20";
-          topDiv.appendChild(minRequired);
-        }
-
-        if (!maxLabelExists) {
-          var maxLabel = document.createElement("label");
-          maxLabel.textContent = "최대 선택 가능 개수: ";
-          maxLabel.className = "maxLabel";
-          topDiv.appendChild(maxLabel);
-        }
-
-        if (!maxRequiredExists) {
-          var maxRequired = document.createElement("input");
-          maxRequired.type = "number";
-          maxRequired.className = "input_maxRequired";
-          maxRequired.min = "1";
-          maxRequired.max = "20";
-          topDiv.appendChild(maxRequired);
-        }
-      }
-    });
+    selectElem.addEventListener("change", surveyTypeChange);
 
     // 답변 추가 버튼 리스너
-    newAnswerBtn.addEventListener("click", function () {
-      // 현재의 답변 유형 텍스트 추출
-      let selectedIndex = selectElem.selectedIndex;
-      let selectedText = selectElem.options[selectedIndex].text;
-      answerCounts[selectedText]++;
-      console.log("answerCounts 상태: " + answerCounts[selectedText]);
-      checkAnswerCount(selectedText, newAnswerBtn);
-
-      // 답변 생성 함수 호출
-      createAnswer(
-        topDiv,
-        formCount1,
-        selectElem.options[selectElem.selectedIndex].text,
-        selectElem,
-        newAnswerBtn
-      );
-    });
+    newAnswerBtn.addEventListener("click", addAnswer);
   });
 }
 
@@ -208,7 +263,7 @@ function createAnswer(
 ) {
   let currentAnswerCounts = JSON.parse(topDiv.dataset.answerCounts);
 
-  if (answerCounts[selectedText] > maxAnswerCounts[selectedText]) {
+  if (currentAnswerCounts[selectedText] > maxAnswerCounts[selectedText]) {
     console.log("최대 답변 개수에 도달했습니다.");
     return;
   }
@@ -237,7 +292,6 @@ function createAnswer(
       break;
 
     case "서술형":
-      defaultAnswerContent(selectElem, topDiv, answerDiv, newAnswerBtn);
       break;
   }
 
@@ -246,6 +300,7 @@ function createAnswer(
 
 // 답변 요소 공통 생성 메소드
 function defaultAnswerContent(selectElem, topDiv, answerDiv, newAnswerBtn) {
+  let currentAnswerCounts = JSON.parse(topDiv.dataset.answerCounts);
   // input type="text" 요소 생성
   var inputElement = document.createElement("input");
   inputElement.type = "text";
@@ -275,7 +330,6 @@ function defaultAnswerContent(selectElem, topDiv, answerDiv, newAnswerBtn) {
     topDiv.dataset.answerCounts = JSON.stringify(currentAnswerCounts);
 
     checkAnswerCount(selectedText, newAnswerBtn);
-    console.log("answerCounts 상태: " + answerCounts[selectedText]);
     topDiv.removeChild(answerDiv);
   });
 
@@ -284,16 +338,32 @@ function defaultAnswerContent(selectElem, topDiv, answerDiv, newAnswerBtn) {
   answerDiv.appendChild(buttonElement);
 
   topDiv.appendChild(answerDiv);
+  topDiv.dataset.currentAnswerCounts = JSON.stringify(currentAnswerCounts); // 저장하기
 }
 
 // let count = 0;
 // bottomDiv 생성 함수
-function createBottomDiv(totalDiv) {
+function createBottomDiv(topDiv, isMandatory) {
   // count++;
 
   // bottomDiv 선언
   let bottomDiv = document.createElement("div");
   bottomDiv.className = "bottomDiv";
+
+  // 문항 삭제버튼
+  let deleteBtn = document.createElement("button");
+  deleteBtn.className = "btn_deleteQuestion";
+  deleteBtn.type = "button";
+  deleteBtn.value = "delete";
+  deleteBtn.innerText = "삭제";
+
+  // 문항 삭제 리스너
+  deleteBtn.addEventListener("click", function () {
+    // totalDiv를 부모 노드에서 삭제
+    topDiv.parentNode.removeChild(topDiv);
+
+    rearrangeQuestionNumbers();
+  });
 
   // 필수여부 체크박스
   let mandatoryCheck = document.createElement("input");
@@ -308,24 +378,12 @@ function createBottomDiv(totalDiv) {
   mandatoryLabel.htmlFor = "ckeck_mandatoryQuestion_id" + formCount1; // 위에서 지정한 id값을 사용
   mandatoryLabel.innerText = "필수여부";
 
-  // 문항 삭제버튼
-  let deleteBtn = document.createElement("button");
-  deleteBtn.className = "btn_deleteQuestion";
-  deleteBtn.type = "button";
-  deleteBtn.value = "delete";
-  deleteBtn.innerText = "삭제";
+  // 필수여부 체크박스 상태 설정
+  mandatoryCheck.checked = isMandatory;
 
-  // 문항 삭제 리스너
-  deleteBtn.addEventListener("click", function () {
-    // totalDiv를 부모 노드에서 삭제
-    totalDiv.parentNode.removeChild(totalDiv);
-
-    rearrangeQuestionNumbers();
-  });
-
+  bottomDiv.appendChild(deleteBtn);
   bottomDiv.appendChild(mandatoryCheck);
   bottomDiv.appendChild(mandatoryLabel);
-  bottomDiv.appendChild(deleteBtn);
 
   return bottomDiv;
 }
