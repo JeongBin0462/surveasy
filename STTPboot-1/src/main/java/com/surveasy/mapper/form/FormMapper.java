e49d05eb4913255b605c2e37d384e84e60ce5a4e
Jisu-Seong <sjangela0615@gmail.com>
2023-08-30 14:58:32 +0900

    설문지제작 mapper 수정중

--- STTPboot-1/src/main/java/com/surveasy/mapper/form/FormMapper.java
+++ STTPboot-1/src/main/java/com/surveasy/mapper/form/FormMapper.java
@@ -2,9 +2,21 @@ package com.surveasy.mapper.form;
 
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import com.surveasy.model.survey.SurveyPaper;
 
 @Mapper
 public interface FormMapper {
-//	@Insert("")
-//	int insertSurvey(@Param("surveytitle") String surveytitle, @Param("surveycontent") String surveycontent, )
+	
+	// 임시저장(regidate, deadline 제외) surveypaper 테이블
+	@Insert("INSERT INTO surveypaper (surveytitle, surveycontent, link) VALUES (#{surveytitle}, #{surveycontent}, #{link})")
+	int insertSurvey(@Param("surveytitle") String surveytitle, @Param("surveycontent") String surveycontent, @Param("link") String link);
+
+	// 저장한 surveypaper의 surveyno를 가져옴
+	@Select("SELECT * FROM surveypaper WHERE surveytitle = #{surveytitle} and surveycontent = #{surveycontent} and link = #{link}")
+	SurveyPaper getSameSurvey(@Param("surveytitle") String surveytitle, @Param("surveycontent") String surveycontent, @Param("link") String link);
+	
+//	@Insert
 }
