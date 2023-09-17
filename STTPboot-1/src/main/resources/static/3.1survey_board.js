8ed71b3319c39a0a476a2507867347ffa8997e7b
LeeJungbin <dlwjdqls783@gmail.com>
2023-09-17 14:10:04 +0900

    3-1 페이지번호

--- STTPboot-1/src/main/resources/static/3.1survey_board.js
+++ STTPboot-1/src/main/resources/static/3.1survey_board.js
@@ -38,12 +38,10 @@ async function redirectToBoardWithParams() {
 function formatDate(dateString) {
 	const date = new Date(dateString);
 
-	const YYYY = date.getFullYear();
 	const MM = String(date.getMonth() + 1).padStart(2, '0');
 	const DD = String(date.getDate()).padStart(2, '0');
 	const HH = String(date.getHours()).padStart(2, '0');
 	const mm = String(date.getMinutes()).padStart(2, '0');
-	const ss = String(date.getSeconds()).padStart(2, '0');
 
 	return `${MM}-${DD} ${HH}:${mm}`;
 }
@@ -82,8 +80,13 @@ function updateSurveyData(data) {
 	} else {
 		nextPageButton.disabled = false;
 	}
-
+	
+	// 현재 페이지 및 전체 페이지 숫자 갱신
+	updatePageNumbers(currentPage, totalPage);
+	
 	// hidden input값 갱신
+	document.querySelector('input[name="currentPage"]').value = data.currentPage || 1;
+	document.querySelector('input[name="totalPage"]').value = data.totalPage;
 	document.querySelector('.survey-search').value = data.search || "";
 	document.querySelector('.survey-subject').value = data.subject || "";
 	document.querySelector('.survey-sort').value = data.sort || "";
@@ -155,6 +158,8 @@ buttons.forEach(button => {
 		event.preventDefault();
 
 		let currentPage = parseInt(document.querySelector('input[name="currentPage"]').value);
+		let totalPage = parseInt(document.querySelector('input[name="totalPage"]').value);
+		
 		console.log("여기오니1 ?" + currentPage);
 		if (currentPage == 0) {
 			currentPage = 1;
@@ -164,15 +169,15 @@ buttons.forEach(button => {
 		if(button.textContent.trim() == '◀' && currentPage > 1) {
 			currentPage--;
 			document.querySelector('input[name="currentPage"]').value = currentPage;
-
+			updateCurrentNumber(currentPage);
 		}
 		// 두 번째 버튼(다음)을 눌렀을 때
-		else if(button.textContent.trim() == '▶') {
+		else if(button.textContent.trim() == '▶' && currentPage < totalPage) {
 			console.log("여기오니?");
 			currentPage++;
 			console.log("여기오니?" + currentPage);
 			document.querySelector('input[name="currentPage"]').value = currentPage;
-
+			updateCurrentNumber(currentPage);
 		}
 		
 		const search = document.querySelector('input[name="search"]').value;
@@ -182,3 +187,65 @@ buttons.forEach(button => {
 		loadPageData(currentPage, search, subject, sort);
 	});
 });
+
+// 현재페이지 강조 업데이트
+function updateCurrentNumber(newPage) {
+    let currentNumber = document.querySelector('.highlight-number');
+    if (currentNumber) {
+        currentNumber.classList.remove('highlight-number');
+        currentNumber.textContent = currentNumber.textContent.trim();
+    }
+
+    let pageNumberDivs = document.querySelectorAll('.page-number-list .page-span');
+    pageNumberDivs.forEach((span, index) => {
+        if ((index + 1) === newPage) {
+            span.classList.add('highlight-number');
+        }
+        span.textContent = index + 1;
+    });
+    document.querySelector('input[name="currentPage"]').value = newPage;
+}
+
+// 현재페이지, 전체페이지 갱신
+function updatePageNumbers(currentPage, totalPage) {
+    const pageNumbersContainer = document.querySelector(".page-number-div");
+
+    // 기존 페이지 숫자 삭제
+    const oldPageNumberLists = pageNumbersContainer.querySelectorAll(".page-number-list");
+    oldPageNumberLists.forEach(list => list.remove());
+
+    // 새 페이지 숫자 생성 및 추가
+    for (let i = 1; i <= totalPage; i++) {
+        const pageNumberListDiv = document.createElement("div");
+        pageNumberListDiv.classList.add("page-number-list");
+
+        const span = document.createElement("span");
+        span.textContent = i;
+        span.classList.add("page-span");
+        if (i === currentPage) {
+            span.classList.add("highlight-number");
+        }
+        pageNumberListDiv.appendChild(span);
+        pageNumbersContainer.appendChild(pageNumberListDiv);
+    }
+}
+
+
+document.querySelector('.page-number-div').addEventListener('click', function(event) {
+    if (event.target.classList.contains('page-span')) {
+        const clickedPage = parseInt(event.target.textContent);
+        redirectToPage(clickedPage);
+    }
+});
+
+function redirectToPage(pageNumber) {
+    // 여기서 currentPage를 pageNumber로 업데이트하고 필요한 로직 수행
+    document.querySelector('input[name="currentPage"]').value = pageNumber;
+    
+    // 이제 기존 로직을 사용해 페이지 데이터를 로드
+    const search = document.querySelector('input[name="search"]').value;
+    const subject = document.querySelector('input[name="subject"]').value;
+    const sort = document.querySelector('input[name="sort"]').value;
+
+    loadPageData(pageNumber, search, subject, sort);
+}
